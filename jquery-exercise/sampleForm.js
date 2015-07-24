21931c859a48f6284a4180d6e21b8ed3767a985b
aleidasarzuri <ale.sarzuri@hotmail.com>
2015-07-24 18:07:53 -0400

    Test

--- jquery-exercise/sampleForm.js
+++ jquery-exercise/sampleForm.js
@@ -10,34 +10,65 @@ var MyForm = function(){
     };
 
     this.genderMF = {
-        'gender':'genderM',
-        'gender':'genderF'
+        'genderM':'M',
+        'genderF':'F'
     };
 
     this.hobbies = {
-        'hobbies':'sky',
-        'hobbies':'run',
-        'hobbies':'eat',
-        'hobbies':'sleep',
-        'hobbies':'read',
-        'hobbies':'code'
+        'hobby1':'ski',
+        'hobby2':'run',
+        'hobby3':'eat',
+        'hobby4':'sleep',
+        'hobby5':'read',
+        'hobby6':'code'
+    };
+    this.choose= {
+        'Press':'press',
+        'Internet':'net',
+        'Word of mouth':'mouth',
+        'Other':'other'
+
     };
 
 
+
+};
+
+MyForm.prototype.create = function(nameTest,emailTest,genderMF,hobbies,choose){
+
+    this.fullname(nameTest);
+    this.email(emailTest);
+    this.myhobbies(hobbies.hobby3);
+    this.gender(genderMF.genderF);
+    this.heard(choose.net);
 
 };
 //options
 //('option[value="press"]')
-MyForm.prototype.gender = function(gender){
-    $('input[type="radio"][name="gender"][id="' + this.gender[gender] + '"]').click()
+MyForm.prototype.fullname = function(name){
+//$('input[name="fullname"]').val("hola")
+    $('input[name="fullname"]').val(name);
+};
+MyForm.prototype.email = function(email){
+//$('input[name="email"]').val("hola")
+    $('input[name="email"]').val(email);
+};
+
+ MyForm.prototype.gender = function(gender){
+    $('input[type="radio"][name="gender"][value="' + this.genderMF[gender] + '"]').click()
 };
 
 
-MyForm.prototype.myhobbies= function(hobie){
+ MyForm.prototype.myhobbies= function(hobby){
     //checkbox
 
-   // $('input[type="checkbox"][value="' + this.hobbies[hobbies] + '"]').click();
-    $('input[type="checkbox"][name="hobbies"][value="' + this.hobbies[hobbies] + '"]').click();
+   //$('input[type="checkbox"][value="' + this.hobbies[hobbies] + '"]').click();
+    //$('input[type="checkbox"][id="hobby1"]').click()
+     $('input[type="checkbox"][value="' + this.hobbies[hobby] + '"]').click();
 };
 
 //$('[name=""]').val(this.beans[bean]);
+MyForm.prototype.heard = function(option) {
+    // $('select[id="heard"]').val("net")
+    $('select[id="heard"]').val(this.choose[option]).click();
+};
